87ad1393f70fdf92f1e4881f29bd2bc402f537b6
yogesh <yogvirus@gmail.com>
2016-09-10 16:15:43 +0530

    nested form added for customer docs remarks added on the form filling page

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,5 +3,5 @@ class Document < ActiveRecord::Base
   has_attached_file :document
   validates_attachment_presence :document
   validates_attachment_size :document, :less_than => 5.megabytes
-  validates_attachment_content_type :document, :content_type => ['application/pdf', 'application/msword', 'text/plain']
+  validates_attachment_content_type :document, :content_type => ['application/pdf', 'application/msword', 'text/plain', "image/jpeg", "image/png" ]
 end
